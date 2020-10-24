cd61ea6ed48563b954b0643a3fd95552520aa1a6
SwethaRajakumar2001 <cs18b037@iittp.ac.in>
2020-10-24 15:44:31 +0530

    Elementary structuring of the site-> Splitting into Header and Body section. Created styles.css (has placeholder background and buttons for now)

---
+++ client/src/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Body(props)
+{
+  return (
+    <p>The Body Here</p>
+  );
+}
+
+export default Body;
---
+++ client/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header(props)
+{
+  return (
+    <header>
+    <h1>GreenHearts</h1>
+    <button className="headerButton">🧚‍♀️</button>
+    <button className="headerButton">🍀</button>
+    </header>
+  );
+}
+
+export default Header;
