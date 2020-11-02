ebb21069404b4deb1e7c81661bad8b7f31f24a7c
SwethaRajakumar2001 <cs18b037@iittp.ac.in>
2020-11-02 11:40:55 +0530

    The Hover text!

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -18,17 +18,17 @@ function Header(props)
   }
 
   var cname = props.PageName==="HomePage"? "headerButton lighter" : "headerButton darker"
-
+  var ghhover= props.PageName!=="HomePage" ? "Click here to navigate to the home page" :  "Welcome to GreenHearts!"
   return (
     <header>
-    <h1 onClick={handleHome}>GreenHearts</h1>
+    <h1 onClick={handleHome} title={ghhover} >GreenHearts</h1>
     {props.LoginStat && <button className={cname} onClick={logout}>LogOut</button>}
-    <button className={cname} onClick={handleLogin}>{props.LoginStat? <i class="material-icons">account_circle</i>  : "Login"}</button>
+    <button className={cname} onClick={handleLogin} title="My Profile">{props.LoginStat? <i class="material-icons">account_circle</i>  : "Login"}</button>
 
-    {props.LoginStat && props.PageName!=="HomePage"  && <button className={cname} onClick={()=>props.Setter("Questionnaire")}><i class="material-icons">assignment_turned_in</i></button>}
-    {props.LoginStat && props.PageName!=="HomePage"  && <button className={cname} onClick={()=>props.Setter("ContestPage")}>🎖</button>}
-    {props.LoginStat && props.PageName!=="HomePage"  && <button className={cname} onClick={()=>props.Setter("MyPlantsPage")}><i class="material-icons">local_florist</i></button>}
-    {props.LoginStat && props.PageName!=="HomePage"  && <button className={cname} onClick={()=>props.Setter("FeedPage")}><i class="material-icons">view_day</i></button>}
+    {props.LoginStat && props.PageName!=="HomePage"  && <button className={cname} onClick={()=>props.Setter("Questionnaire")} title="Questionnaire"><i class="material-icons">assignment_turned_in</i></button>}
+    {props.LoginStat && props.PageName!=="HomePage"  && <button className={cname} onClick={()=>props.Setter("ContestPage")} title="Contests">🎖</button>}
+    {props.LoginStat && props.PageName!=="HomePage"  && <button className={cname} onClick={()=>props.Setter("MyPlantsPage")} title="My Plants"><i class="material-icons">local_florist</i></button>}
+    {props.LoginStat && props.PageName!=="HomePage"  && <button className={cname} onClick={()=>props.Setter("FeedPage")} title="Feed"><i class="material-icons">view_day</i></button>}
     </header>
   );
 }
